4fdfbd60db47cd16bddb3a4d7d1ad7e9a30e11b2
ksj5551d-cyber <ksj5551d@gmail.com>
2025-09-18 13:01:35 +0530

    js addition

---
+++ script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/**
+ * navbar toggle
+ */
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  header.classList.toggle("active");
+});
+
+
+
+/**
+ * show go top btn when scroll window to 500px
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 500 ? goTopBtn.classList.add("active")
+    : goTopBtn.classList.remove("active");
+});
+
+/**  
+ * show go bottom btn when scroll window 
+ */
+
+
+const goBottomBtn = document.getElementById('goBottomBtn');
+
+  window.addEventListener('scroll', () => {
+    // Show button only when not near bottom
+    if (window.innerHeight + window.scrollY < document.body.offsetHeight - 200) {
+      goBottomBtn.classList.add('active');
+    } else {
+      goBottomBtn.classList.remove('active');
+    }
+  });
+
+  goBottomBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: 'smooth'
+    });
+  });
